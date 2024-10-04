233ceebea8fdea49349f2d4c641a398a002c0773
Martin George <martingeorge191110@gmail.com>
2024-10-04 13:03:52 +0300

    new Feature: Backend (Notifications status)

--- server/controllers/donationController.js
+++ server/controllers/donationController.js
@@ -1,7 +1,6 @@
 import prisma from "../prisma/prisma.js";
 import { respSuccess } from "../Utilis/authUtility.js";
 import { createError } from "../Utilis/createError.js";
-import { retriveUserCampaign } from "./campaignController.js";
 
 
 /**
@@ -40,7 +39,7 @@ const donate = async (req, res, next) => {
 }
 
 /**
- * viewDonations, This Controller aim to View all donations for user Campaign,
+ * viewDonations, This Controller aim to View all donations for one user Campaign,
  *                this authorized to User who has the authority of this Campaign
  *
  * Description:
@@ -70,7 +69,8 @@ const viewDonations = async (req, res, next) => {
          select: {
             id: true,
             amout: true,
-            userId: true
+            userId: true,
+            createdAt: true
             , author: {
                select: {
                   f_name: true, l_name: true, gender: true, avatar: true, email: true
@@ -84,7 +84,7 @@ const viewDonations = async (req, res, next) => {
 
       const result = donations.map((donation) => {
          return ({
-            id: donation.id, userId: donation.userId, amount: donation.amout, ...donation.author
+            id: donation.id, createdAt: donation.createdAt, userId: donation.userId, amount: donation.amout, ...donation.author
          })
       })
 
@@ -94,7 +94,95 @@ const viewDonations = async (req, res, next) => {
    }
 }
 
+/**
+ * viewDonations, This Controller aim to View all Campaign donations status,
+ *                which not seen by campaign author
+ *
+ * Description:
+ *             [1] --> get user id after verify token
+ *             [2] --> check whether any donations did not be seen by campaign author or not
+ *             [3] --> Reponse
+ */
+
+const DonationsSeenByAuhor = async (req, res, next) => {
+   const {id} = req
+
+   try {
+      const donations = await prisma.donation.findMany({
+         where: {
+            campaign: {
+               userId: id,
+            },
+            seenByAuthor: false
+         },
+         select: {
+            amout: true,
+            author: {
+               select: {
+                  id: true,
+                  f_name: true,
+                  l_name: true,
+                  email: true
+               }
+            },
+            createdAt: true,
+            seenByAuthor: true
+         }
+      })
+
+      if (!donations, donations.length === 0)
+         return (next(createError("No natification sent yet!", 404)))
+
+      const result = donations.map((donation) => {
+         return ({
+            amount: donation.amout, createdAt: donation.createdAt, seenByAuthor: donation.seenByAuthor,
+            ...donation.author
+         })
+      })
+
+      return (respSuccess(res, 200, "Operation, Successed!", "New Notification!", donations))
+   } catch (err) {
+      return (next(createError("Something went wrong!", 500)))
+   }
+}
+
+/**
+ * updateSeenByAuthor, Controller to update notification status after user seen the dons
+ * 
+ * Description:
+ *             [1] --> get user id after verify token
+ *             [2] --> check whether any donations did not be seen by campaign author or not and update it
+ *             [3] --> Reponse
+ */
+
+const updateSeenByAuthor = async (req, res, next) => {
+   const {id} = req
+
+   try {
+      const donations = await prisma.donation.updateMany({
+         where: {
+            campaign: {
+               userId: id
+            },
+            seenByAuthor: false
+         },
+         data: {
+            seenByAuthor: true
+         }
+      })
+
+      if (!donations || donations.count === 0)
+         return (next(createError("No notification yet!", 404)))
+
+      return (respSuccess(res, 200, "Operation, Successed!", "Notifications Updated!", null))
+   } catch (err) {
+      return (next(createError("Something went wrong!", 500)))
+   }
+}
+
 export {
    donate,
-   viewDonations
+   viewDonations,
+   DonationsSeenByAuhor,
+   updateSeenByAuthor
 }
